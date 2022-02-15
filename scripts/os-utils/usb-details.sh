87402860b1bece918b4ec310e84568c7cde8ba15
mattlennon3 <mattlennon3@hotmail.co.uk>
2022-02-15 21:41:58 +0000

    Cleanup linux things - dont need to see on mac

--- scripts/os-utils/usb-details.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Created on 2020-12-31 by Matt Lennon
-#
-
-# https://wiki.archlinux.org/index.php/PCI_passthrough_via_OVMF#USB_controller
-
-for usb_ctrl in /sys/bus/pci/devices/*/usb*; do pci_path=${usb_ctrl%/*}; iommu_group=$(readlink $pci_path/iommu_group); echo "Bus $(cat $usb_ctrl/busnum) --> ${pci_path##*/} (IOMMU group ${iommu_group##*/})"; lsusb -s ${usb_ctrl#*/usb}:; echo; done
-  
-
